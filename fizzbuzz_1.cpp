b505eedca473a52c52152babb074700e8f08fef2
Diba Mirza <dimirza@cs.ucsb.edu>
2018-01-18 16:46:22 -0800

    lecture code

--- fizzbuzz_1.cpp
+++ fizzbuzz_1.cpp
@@ -1,15 +1,22 @@
 //fizzbuzz_0.cpp
 //Diba Mirza 01/18
+//Demonstrate passing arguments to the main function
+// or passing command line arguments
 
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(int argc, char * argv[]){
 	int num; //Variable declaration. In C++ you must always
 			 // declare your variables before using them
-	cout<<"Enter a number: ";
-	cin >> num; 
-	cout<<"You entered: "<< num<<endl;
+			 // Uninitialized variables have junk values (not zero)
+
+	if (argc != 2){
+		cerr<<"Usage "<<argv[0]<< " <number> \n";
+		exit(1);
+	}
+
+	num = atoi(argv[1]);
 
 	if (num%3 == 0 && num%5 ==0 ){
 		cout<<"fizzbuzz"<<endl;	
---
+++ fizzbuzz_2.cpp
@@ -0,0 +1,37 @@
+//fizzbuzz_0.cpp
+//Diba Mirza 01/18
+//Demonstrate passing arguments to the main function
+// or passing command line arguments
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]){
+	int limit = 0;
+	int num; //Variable declaration. In C++ you must always
+			 // declare your variables before using them
+			 // Uninitialized variables have junk values (not zero)
+
+	if (argc != 2){
+		cerr<<"Usage "<<argv[0]<< " <number> \n";
+		exit(1);
+	}
+
+	limit = atoi(argv[1]);
+
+	for (num =0; num <=limit; num++){
+		if (num%3 == 0 && num%5 ==0 ){
+			cout<<"fizzbuzz"<<endl;	
+		
+		} else if(num%3 == 0){
+			cout<<"fizz"<<endl;	
+
+		} else if(num%5 == 0){
+			cout<<"buzz"<<endl;	
+		} else {
+			cout<<num<<endl;	
+		}
+	}
+
+	return 0;
+}
